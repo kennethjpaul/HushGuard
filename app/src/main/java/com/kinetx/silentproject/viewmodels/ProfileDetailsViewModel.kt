956c0c19ba84782f725e1d1b42aa36ddbf04455a
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2023-05-07 20:35:28 +0200

    Rolled back on the version used for certain dependencies. Implemented the initial layout for profile details

--- app/src/main/java/com/kinetx/silentproject/viewmodels/ProfileDetailsViewModel.kt
+++ app/src/main/java/com/kinetx/silentproject/viewmodels/ProfileDetailsViewModel.kt
@@ -1,6 +1,7 @@
 package com.kinetx.silentproject.viewmodels
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -12,16 +13,29 @@ class ProfileDetailsViewModel(application: Application, private val argList : Pr
     val fragmentTitle : LiveData<String>
         get() = _fragmentTitle
 
+    private var _isCreateVisible = MutableLiveData<Int>()
+    val isCreateVisible : LiveData<Int>
+        get() = _isCreateVisible
+
+
+    private var _isUpdateVisible = MutableLiveData<Int>()
+    val isUpdateVisible : LiveData<Int>
+        get() = _isUpdateVisible
+
 
     init {
 
         if (argList.profileId==-1L)
         {
             _fragmentTitle.value = "Create Profile"
+            _isCreateVisible.value = View.VISIBLE
+            _isUpdateVisible.value = View.GONE
         }
         else
         {
             _fragmentTitle.value = "Create Profile"
+            _isCreateVisible.value = View.GONE
+            _isUpdateVisible.value = View.VISIBLE
         }
 
 
